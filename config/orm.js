a52a521ac7fc5d415243e46a0f0e3b03e03e269a
Kay0s <hamilton.kristina@gmail.com>
2021-01-06 11:57:54 -0600

    updated selectAll(), insertOne(), updateOne() on orm.js

--- config/orm.js
+++ config/orm.js
@@ -39,7 +39,7 @@ function objToSql(ob) {
 }
 
 let orm = {
-  all: function (tableInput, cb) {
+  selectAll: function (tableInput, cb) {
     let queryString = "SELECT * FROM " + tableInput + ";";
     connection.query(queryString, function (err, result) {
       if (err) {
@@ -48,7 +48,7 @@ let orm = {
       cb(result);
     });
   },
-  create: function (table, cols, vals, cb) {
+  insertOne: function (table, cols, vals, cb) {
     let queryString = "INSERT INTO " + table;
 
     queryString += " (";
@@ -69,7 +69,7 @@ let orm = {
     });
   },
 
-  update: function (table, objColVals, condition, cb) {
+  updateOne: function (table, objColVals, condition, cb) {
     let queryString = "UPDATE " + table;
 
     queryString += " SET ";
